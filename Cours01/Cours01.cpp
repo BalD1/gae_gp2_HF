48b7eccb92941513e6e5b72b44d2f6e126a5fd2f
Florian Husson <florianhusson@hotmail.fr>
2021-09-21 16:50:01 +0200

    bugfix

--- Cours01/Cours01.cpp
+++ Cours01/Cours01.cpp
@@ -10,8 +10,7 @@ int main()
 	printf("0.5: %4.1f \n", 0.5);
 	printf("toto: %s \n", "toto");
 
-	
-	const char* str = "";
+	printf("1...9x2: ");
 	for (int j = 0; j < 2; j++)
 	{
 		for (int i = 1; i < 10; i++)
