bbef32fa18ecb1e46090bd44cbe291269cb8b5cc
Florian Husson <florianhusson@hotmail.fr>
2021-11-09 15:07:14 +0100

    09/11 ground + montain

--- SFML_Cours04/Cours04_SFML/Cours04/Cours04.cpp
+++ SFML_Cours04/Cours04_SFML/Cours04/Cours04.cpp
@@ -31,6 +31,7 @@ float enemySpeed = 2;
 #pragma endregion
 
 sf::Vector2f windowCenter;
+sf::Vector2f windowSize;
 sf::Vector2i mousePos;
 
 std::vector<sf::CircleShape> projVec;
@@ -49,12 +50,15 @@ void EnemyMovements();
 void Fire();
 void ProjectilesBehaviour();
 void SetProjectile(sf::CircleShape* projectile);
+void DrawGround(sf::RenderWindow& window);
+void DrawMountain(sf::RenderWindow& window);
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(1280, 720, 64), "wesh la mif c'est moi la fenetre de ouf");
 	window.setFramerateLimit(60);
 	window.setVerticalSyncEnabled(true);
+	windowSize = sf::Vector2f(window.getSize().x, window.getSize().y);
 
 	windowCenter = sf::Vector2f(window.getSize().x / 3, window.getSize().y / 4);
 
@@ -64,7 +68,7 @@ int main()
 
 #pragma region Player
 
-	playerHead = SetCircle(100, sf::Color::White, windowCenter);
+	playerHead = SetCircle(60, sf::Color::White, sf::Vector2f(0,410));
 	playerHead.move(100, 100);
 
 	sf::Vector2f playerHPOffset = sf::Vector2f(-25, -50);
@@ -72,13 +76,13 @@ int main()
 
 	inv_Timer = invincibilityCD;
 
-	eyeL = SetCircle(20, sf::Color::Blue, windowCenter);
+	eyeL = SetCircle(12, sf::Color::Blue, sf::Vector2f(0, 410));
 	eyeL.move(130, 140);
 	
-	eyeR = SetCircle(20, sf::Color::Blue, windowCenter);
+	eyeR = SetCircle(12, sf::Color::Blue, sf::Vector2f(-40, 410));
 	eyeR.move(220, 140);
 
-	canon = SetRectangle(100, 20, sf::Color::Red, windowCenter);
+	canon = SetRectangle(60, 20, sf::Color::Red, sf::Vector2f(0, 370));
 	canon.move(100, 210);
 
 #pragma endregion
@@ -111,7 +115,7 @@ int main()
 
 	}
 	sf::Text title = SetText(50, "CYBERPUNK 2077", sf::Color::Yellow, cybrpnukFont);
-	title.setFont(cybrpnukFont);
+	//title.setFont(cybrpnukFont);
 
 	sf::Text endText = SetText(70, "", sf::Color::Yellow, cybrpnukFont);
 	endText.setFont(cybrpnukFont);
@@ -142,7 +146,7 @@ int main()
 
 			CanonRotation();
 
-			EnemyMovements();
+			//EnemyMovements();
 
 			enemyHP.setPosition(enemy.getPosition() + enemyHPOffset);
 			playerHP.setPosition(playerHead.getPosition() + playerHPOffset);
@@ -188,6 +192,9 @@ int main()
 
 #pragma region Draws
 
+		DrawGround(window);
+		DrawMountain(window);
+
 		window.draw(playerHead);
 		window.draw(eyeL);
 		window.draw(eyeR);
@@ -199,7 +206,7 @@ int main()
 			window.draw(projVec[i]);
 		}
 		window.draw(canon);
-		window.draw(title);
+		//window.draw(title);
 
 		if (gameEnd)
 			window.draw(endText);
@@ -211,7 +218,6 @@ int main()
 
 }
 
-
 void ProcessInputs()
 {
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
@@ -220,11 +226,13 @@ void ProcessInputs()
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
 		Movements(1, 0);
 
+	/*
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up))
 		Movements(0, -1);
 
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down))
 		Movements(0, 1);
+		*/
 
 	if (sf::Mouse::isButtonPressed(sf::Mouse::Button(0)))
 	{
@@ -335,4 +343,61 @@ void SetProjectile(sf::CircleShape *projectile)
 	projectile->setFillColor(sf::Color::Green);
 	projectile->setRotation(canon.getRotation());
 	projectile->setPosition(canon.getPosition());
+}
+
+void DrawGround(sf::RenderWindow& window)
+{
+	sf::VertexArray vArr = sf::VertexArray(sf::LinesStrip);
+	
+	sf::Color groundColor = sf::Color(sf::Uint8(145), sf::Uint8(145), sf::Uint8(0));
+	float baseLine = 600 + 32;
+
+	sf::Vector2f a(0, baseLine);
+	sf::Vector2f b(windowSize.x, baseLine);
+
+	vArr.append(sf::Vertex(a, groundColor));
+	vArr.append(sf::Vertex(b, groundColor));
+
+	window.draw(vArr);
+}
+
+void DrawMountain(sf::RenderWindow& window)
+{
+	sf::VertexArray vArr = sf::VertexArray(sf::LinesStrip);
+
+	sf::Color mountainColor = sf::Color(sf::Uint8(200), sf::Uint8(99), sf::Uint8(70));
+	float baseLine = 610 - 32;
+
+
+
+	sf::Vector2f a(0, baseLine);
+	sf::Vector2f b(400, baseLine - 330);
+	sf::Vector2f c(800, baseLine - 400);
+	sf::Vector2f d(windowSize.x, baseLine);
+	
+	for (int i = 0; i < 256; i++)
+	{
+		float t = 1.0f * i / 256;
+		float x = catmul(a.x, a.x, b.x, c.x, t);
+		float y = catmul(a.y, a.y, b.y, c.y, t);
+
+		vArr.append(sf::Vertex(sf::Vector2f(x, y), mountainColor));
+	}
+	for (int i = 0; i < 256; i++)
+	{
+		float t = 1.0f * i / 256;
+		float x = catmul(a.x, b.x, c.x, d.x, t);
+		float y = catmul(a.y, b.y, c.y, d.y, t);
+
+		vArr.append(sf::Vertex(sf::Vector2f(x,y), mountainColor));
+	}	
+	for (int i = 0; i < 256; i++)
+	{
+		float t = 1.0f * i / 256;
+		float x = catmul(b.x, c.x, d.x, d.x, t);
+		float y = catmul(b.y, c.y, d.y, d.y, t);
+
+		vArr.append(sf::Vertex(sf::Vector2f(x, y), mountainColor));
+	}
+	window.draw(vArr);
 }
--- SFML_Cours04/Cours04_SFML/Cours04/Utility.hpp
+++ SFML_Cours04/Cours04_SFML/Cours04/Utility.hpp
@@ -2,6 +2,8 @@
 
 #include <SFML/Graphics.hpp>
 
+#pragma region Vectors
+
 sf::Vector2f Vector2zero()
 {
 	sf::Vector2f v = sf::Vector2f(0, 0);
@@ -66,4 +68,18 @@ sf::Vector2f DivVectors(sf::Vector2f a, float b)
 	a.y /= b;
 
 	return a;
+}
+
+#pragma endregion
+
+float catmul(float p0, float p1, float p2, float p3, float t)
+{
+	float q = p1 * 2.0f;
+	float t2 = t * t;
+
+	q += (-p0 + p2) * t;
+	q += (2.0f * p0 - 5.0f * p1 + 4.0f * p2 - p3) * t2;
+	q += (-p0 + 3.0f * p1 - 3 * p2 + p3) * t2 * t;
+
+	return 0.5 * q;
 }
